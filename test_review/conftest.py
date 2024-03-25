0540a3b214e6f4937c04421af1a32ea0c8726f74
Artem Dedkov <bronito@yandex-team.ru>
2024-03-25 17:14:15 +0700

    Обновил файл conftest.py

--- test_review/conftest.py
+++ test_review/conftest.py
@@ -2,18 +2,21 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-options = Options()
-options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-
-
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default=None,
                      help="Choose browser: chrome or firefox")
-
-
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose language: '--language=en' or '--language=ru' or '--language=es'")               
+    
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
+
+    options = Options()
+    options.add_experimental_option(
+        'prefs', {'intl.accept_languages': user_language})
+
     browser = None
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
